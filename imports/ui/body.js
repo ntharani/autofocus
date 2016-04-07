b51c248e0c27675db68dd00cd556fae1bcd44a9f
Nagib Tharani <ntharani@gmail.com>
2016-04-07 11:26:43 -0700

    Altered forms logic and removed older code

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,70 +1,73 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
 import { Categories } from '../api/categories.js';
 
- 
+
 import './body.html';
 import './task.js';
 
- 
+Template.body.onCreated(function bodyOnCreated() {
+	this.state = new ReactiveDict();
+  this.state.set('task-input', 'task');
+});
+
+
 Template.body.helpers({
-tasks() {
+	tasks() {
+		const instance = Template.instance();
+		if (instance.state.get('hideCompleted')) {
+			//If 'Hide Completed' is checked, filter the tasks
+			return Tasks.find({ checked: {$ne: true}}, {sort: {createdAt: -1}});
+		}
+		// Otherwise, return all the tasks
     return Tasks.find({}, { sort: { createdAt: -1 }});
   },
 
- categories() {
- 		return Categories.find({},  { sort: { createdAt: -1 }} );
- }
+  incompleteCount() {
+  	return Tasks.find({ checked: { $ne: true}}).count();
+  },
+
+  categories() {
+   return Categories.find({},  { sort: { createdAt: -1 }} );
+ },
+  
+  currentState(state) {
+    return Template.instance().state.get('task-input') === state;
+  }
 
 });
 
 Template.body.events({
-  'submit .new-task'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
+	'change #taskz'({target: {value}}, {state}) {
+		const current = state.get('task-input');
 
-    // Get value from form element
-    const target = event.target;
-    const text = target.text.value;
- 
-    // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
-    $('.new-task').velocity("fadeOut", {duration:250});
+		if (current === 'task') {
+			// Insert a task into the collection
+     Tasks.insert({
+       text: value,
+	      createdAt: new Date(), // current time
+	    });
+     $('#taskz').val('');
+      state.set('task-input', 'category');
+   } else if (current === 'category') {
+			// Insert a task into the collection
+			Categories.insert({
+       text: value,
+	      createdAt: new Date(), // current time
+	    });
+      $('#taskz').val('');
+      state.set('task-input', 'task');
 
-    // Clear form
-    target.text.value = '';
-    $('.category').velocity("fadeIn", {duration:250});
-    $('.category').focus();
+		}
 
-  },
+		// if (keyCode === 13) {
 
-    'submit .category'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
+		// }
+	},
 
-    // Get value from form element
-    const target = event.target;
-    const text = target.ctext.value;
- 
-    // Insert a task into the collection
-    Categories.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
-    
-    target.ctext.value = '';
-    $('.category').velocity("fadeOut", {duration:250}, function() {$('.new-task').velocity("fadeIn", {duration:250})});
-    console.log("Faded Out Category");
-    // Clear form
-    //$('.category').velocity("fadeIn", {duration:250});
-    console.log("Faded In Category");
-    $('.new-task').velocity("fadeIn", {duration:250});
-    $('.new-task').focus();
-
-
+  'change .hide-completed input'(event, instance) {
+  	instance.state.set('hideCompleted', event.target.checked);
   },
 
 
